1159910057f88bb988af07c3f3145faa90937c3f
Brenda Garcia <brendagar88@gmail.com>
2021-01-20 14:05:31 -0600

    Muestra todas las evoluciones, aún de pokemon que no están en la data

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ import data from './data/pokemon/pokemon.js';
 let allPokemon = data.pokemon;
 
 export function searchPokemon(data, pokeToSearch) {
-  let pokemonFounded = data.filter((searchName) => {
+  let pokemonFounded = data.find((searchName) => {
     return searchName.name === pokeToSearch;
   });
   return pokemonFounded;
@@ -12,15 +12,18 @@ export function searchPokemon(data, pokeToSearch) {
 export let searchEvolutions = (pokemon, evolName, array) => {
   function filterPokeEv (pokemon){
     let evolution = pokemon.evolution;
+    console.log(evolution);
     if (Object.keys(evolution).includes(evolName)) {
       let nextEvName = evolution[evolName][0]['name'];
-      let nextEv = allPokemon.filter((pokemon) => pokemon.name === nextEvName);
-      if (evolName == 'next-evolution') {
-        array.push(nextEv);
-        filterPokeEv(nextEv[0]);
-      } else {
-        array.unshift(nextEv);
-        filterPokeEv(nextEv[0]);
+      let nextEv = allPokemon.find((pokemon) => pokemon.name === nextEvName);
+      if (nextEv !== undefined) {
+        if (evolName == 'next-evolution') {
+          array.push(nextEv);
+          filterPokeEv(nextEv);
+        } else {
+          array.unshift(nextEv);
+          filterPokeEv(nextEv);
+        }
       }
     }
   }
