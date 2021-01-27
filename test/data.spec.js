9a82907e6d348788373f21d7828d3096f0f5ae93
Brenda Garcia <brendagar88@gmail.com>
2021-01-26 18:44:11 -0600

    Filtra por buddy-distance y test 100%

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 import { objAllPokemon, nextEvolutions, prevEvolutions, searchPokemon, searchEvolutions} from '../src/data.js';
 
-=======
-import { objAllPokemon, nextEvolutions, searchPokemon, searchEvolutions } from '../src/data.js';
-
-
-
->>>>>>> 2447012e8214a6913550cb5ed410a72c0466c3c2
 describe('searchPokemon', () => {
   it('is a function', () => {
     expect(typeof searchPokemon).toBe('function');
   });
 
-<<<<<<< HEAD
   it('should return `pokemonFounded`', () => {
     expect(searchPokemon(objAllPokemon, 'charizard')).toBe(objAllPokemon[5]);
     expect(searchPokemon(objAllPokemon, 'bulbasaur')).toBe(objAllPokemon[0]);
-=======
-  it('returns `pokemonFounded`', () => {
-    expect(searchPokemon(objAllPokemon, 'charizard')).toBe(objAllPokemon[5]);
->>>>>>> 2447012e8214a6913550cb5ed410a72c0466c3c2
   });
 });
 
@@ -29,7 +17,6 @@ describe('searchEvolutions', () => {
     expect(typeof searchEvolutions).toBe('function');
   });
 
-<<<<<<< HEAD
   it('should return `evolutionsArray`', () => {
     expect(searchEvolutions(objAllPokemon[0],'next-evolution', nextEvolutions)).toStrictEqual([objAllPokemon[1], objAllPokemon[2]]);
     expect(searchEvolutions(objAllPokemon[5],'prev-evolution', prevEvolutions)).toStrictEqual([objAllPokemon[3], objAllPokemon[4]]);
@@ -38,10 +25,6 @@ describe('searchEvolutions', () => {
     expect(searchEvolutions(objAllPokemon[5],'next-evolution', nextEvolutions)).toStrictEqual([]);
     expect(searchEvolutions(objAllPokemon[3],'prev-evolution', prevEvolutions)).toStrictEqual([]);
     expect(searchEvolutions(objAllPokemon[110],'next-evolution', nextEvolutions)).toStrictEqual([objAllPokemon[111]]);
-=======
-  it('returns `nextEvolutionsArray`', () => {
-    expect(searchEvolutions(objAllPokemon[0],'next-evolution', nextEvolutions)).toStrictEqual([objAllPokemon[1], objAllPokemon[2]]);
->>>>>>> 2447012e8214a6913550cb5ed410a72c0466c3c2
   });
 });
 
